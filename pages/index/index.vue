378ae438f20fd87992be1faf4bada7a1ba566ccb
辶开呀 <914044753@qq.com>
2023-11-13 10:18:40 +0800

    配置 uView V2.0.36

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="content">
+		XXXXX
+    <u-button text="渐变色按钮" color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"></u-button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
