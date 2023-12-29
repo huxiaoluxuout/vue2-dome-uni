20f8826754b39a41f1d9e3ff12c2d15712436798
辶开呀 <914044753@qq.com>
2023-12-29 17:59:02 +0800

    测试定位授权

--- pages/tabbar1/tabbar1.vue
+++ pages/tabbar1/tabbar1.vue
@@ -1,30 +1,59 @@
 <template>
-	<view>
-000
+  <view>
+    000
 
-	 <button @click="navigateTo('pages/A1/A1')"> 页面1</button>
-	 
+    <button @click="navigateTo('pages/A1/A1')"> 页面1</button>
+    <button @click="testHandler" style="margin-top: 20px;">GPS</button>
+
     <tabbar :INDEX="0"></tabbar>
 
-	</view>
+  </view>
 </template>
 
 <script>
-	import ZshuNavbar from "@/components/zshu-components/zshu-navbar.vue";
-  import {navigateTo} from "@/utils/tools";
+import ZshuNavbar from "@/components/zshu-components/zshu-navbar.vue";
+import {navigateTo} from "@/utils/tools";
+import permision from "@/js_sdk/wa-permission/permission.js"
 
-  export default {
-    components: {ZshuNavbar},
-		data() {
-			return {
+export default {
+  components: {ZshuNavbar},
+  data() {
+    return {}
+  },
+  methods: {
+    navigateTo,
 
-			}
-		},
-		methods: {
-      navigateTo
+    testHandler() {
+      this.requestAndroidPermission('android.permission.ACCESS_FINE_LOCATION')
+     /* let result = permision.checkSystemEnableLocation() //返回true或false
+      console.log(result)
+      if(!result){
+        permision.gotoAppPermissionSetting()
+      }*/
+      /*let result = permision.judgeIosPermission("location")
+      console.log(result)*/
+    },
+    // vue的method里编写如下代码
+    async requestAndroidPermission(permisionID) {
+      var result = await permision.requestAndroidPermission(permisionID)
+      var strStatus
+      if (result == 1) {
+        strStatus = "已获得授权"
+      } else if (result == 0) {
+        strStatus = "未获得授权"
+      } else if (result == -1){
+        strStatus = "被永久拒绝权限"
+         // let result = permision.checkSystemEnableLocation() //返回true或false
+         console.log(result)
+         if(result == -1){
+           permision.gotoAppPermissionSetting()
+         }
+      }
+      console.log(strStatus)
 
-		}
-	}
+    },
+  }
+}
 </script>
 
 <style>
