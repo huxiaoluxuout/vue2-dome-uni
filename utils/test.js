9149a10da01072ded887e39ae325795e97f6694b
辶开呀 <914044753@qq.com>
2024-01-30 11:14:27 +0800

    uni-id-pages

--- utils/test.js
+++ utils/test.js
@@ -1,15 +1,18 @@
 export const getCID = () => {
-    // #ifdef APP-PLUS
-    uni.getPushClientId({
-        success(res) {
-            console.log('getPushClientId',res.cid);
-        },
-        fail(err) {
-            console.log(err)
-        }
-    })
+	// #ifdef APP-PLUS
+	uni.getPushClientId({
+		success(res) {
+			console.log('getPushClientId', res.cid);
+		},
+		fail(err) {
+			console.log(err)
+		}
+	})
+	uni.onPushMessage((res) => {
+		console.log("收到推送消息：", res) //监听推送消息
+	})
 
 
-    // #endif
+	// #endif
 
 }
